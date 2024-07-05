5933ed3405bcb2173aae5300d562ce11f239120d
gustavon_brito <gustavoserafio@gmail.com>
2024-07-05 00:23:53 -0300

    chore: adicionando dependencias do spring boot, como ORM, dev tools, validation para dto, lombok para boilers, flyway para migration

---
+++ src/test/java/com/codigoCerto/desafioBackEnd/DesafioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigoCerto.desafioBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
