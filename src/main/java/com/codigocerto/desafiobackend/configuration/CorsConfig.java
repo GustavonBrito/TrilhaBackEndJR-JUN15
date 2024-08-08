9d27568676b62785ed80906b110cb184e20434ad
GustavonBrito <gustavoserafio@gmail.com>
2024-08-08 09:53:57 -0300

    feat: Railway deploy

--- src/main/java/com/codigocerto/desafiobackend/configuration/CorsConfig.java
+++ src/main/java/com/codigocerto/desafiobackend/configuration/CorsConfig.java
@@ -1,19 +1,30 @@
 package com.codigocerto.desafiobackend.configuration;
 
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("My API").version("1.0"))
+                .info(new Info().title("My API V01").version("1.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .addServersItem(new Server().url("https://trilhabackendjr-jun15-production-352a.up.railway.app"));
     }
 
